2c33941c8c18e26ceaeda436302286a92206650c
Scott Diallo <diallot@me.com>
2016-05-15 15:42:43 -0700

    created a btn and added some styling to the html

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $('document').ready(function () {
     for (var i = 0; i <= 100; i++) {
-        var f = i % 3 == 0,
-            b = i % 5 == 0;
-        console.log(f ? b ? 'fizzBuzz' : 'Fizz' : b ? 'buzz' : i);
+        var divisibleByThree = i % 3 == 0,
+            divisibleByfive = i % 5 == 0
+        console.log(divisibleByThree ? divisibleByfive ? "FizzBuzz" : "Fizz" : divisibleByfive ? "Buzz" : i);
     }
 });
