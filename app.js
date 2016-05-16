fa5df7ed9d06eaecb15b098e331ecced14656802
Scott Diallo <diallot@me.com>
2016-05-15 18:45:48 -0700

    append method applied to display results on DOM

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 $('document').ready(function () {
-    for (var i = 0; i <= 100; i++) {
-        var divisibleByThree = i % 3 == 0,
-            divisibleByfive = i % 5 == 0
-        console.log(divisibleByThree ? divisibleByfive ? "FizzBuzz" : "Fizz" : divisibleByfive ? "Buzz" : i);
-    }
+    $('#btn').on('click', function () {
+        var itemValue = $('.itemValue').val();
+        $('#displayResults').empty();
+        for (var i = 1; i <= itemValue; i++) {
+            var divisibleByThree = i % 3 == 0,
+                divisibleByfive = i % 5 == 0;
+            //            console.log(divisibleByThree ? divisibleByfive ? "FizzBuzz" : "Fizz" : divisibleByfive ? "Buzz" : i);
+            var output = divisibleByThree ? divisibleByfive ? "FizzBuzz" : "Fizz" : divisibleByfive ? "Buzz" : i;
+            $('#displayResults').append('<li>' + output + '</li>');
+
+        }
+    });
 });
